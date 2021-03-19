f904eb521f2aaafba6bb3849a1677e38d68d8807
252648 <252648@wezon.co.kr>
2021-03-19 18:43:52 +0900

    0319-18

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
             data : {
                 data : {movies}
             }
-        } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+        } = await axios.get("");
         this.setState({ movies, isLoading : false })
     };
     componentDidMount(){
